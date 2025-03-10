5008cbf4717f88954f771a963736ea01a686ffa6
Casielim <e1300513@u.nus.edu>
2025-03-10 19:00:20 +0800

    Edit checkstyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a Remark with the specified remark text.
+     *
+     * @param remark A non-null string representing the remark for a person.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
